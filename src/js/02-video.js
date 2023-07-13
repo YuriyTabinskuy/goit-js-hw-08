49ee42316a18e6d47c4fa625912d797d06c7e0c9
Yuriy <yuriylviv2006@gmail.com>
2023-07-13 10:07:39 +0300

    complete2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,7 @@ import throttle from 'lodash.throttle';
 // key for videoplayer current time
 const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
-// select iframe from html
+// select iframe from HTML
 const iframeEl = document.querySelector('#vimeo-player');
 
 // create player
@@ -13,10 +13,18 @@ const player = new Player(iframeEl);
 // track timeupdate using throttle - once per 1 second
 player.on('timeupdate', throttle(onPlay, 1000));
 
-// save video current time to the local storage
+// save video current time to local storage
 function onPlay({ seconds }) {
   localStorage.setItem(CURRENT_TIME_KEY, seconds);
 }
 
-// set current time to the videoplayer using local storage saved item
-player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY));
+// retrieve current time from local storage
+const storedTime = localStorage.getItem(CURRENT_TIME_KEY);
+
+// set current time to the video player using the stored value
+if (storedTime !== null) {
+  const currentTime = parseFloat(storedTime);
+  if (!isNaN(currentTime)) {
+    player.setCurrentTime(currentTime);
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,13 +5,13 @@ const STORAGE_KEY = 'feedback-form-state';
 
 // select feedback form and destructuring email and message from form elements
 const formEl = document.querySelector('.feedback-form');
-const { email, message } = formEl.elements;
+// const { email, message } = formEl.elements;
 
 // create form data variable
-let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-
+// let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+let fomDate = {}
 // recovery of input data
-populateInputData();
+// populateInputData();
 
 // add listeners for the input fields and submit button
 formEl.addEventListener('input', throttle(onDataInput, 500));
@@ -19,28 +19,41 @@ formEl.addEventListener('submit', onFormSubmit);
 
 // save input data to the local storage and formData var
 function onDataInput() {
-  formData = { email: email.value, message: message.value };
+  // formData = { email: email.value, message: message.value };
+  formData[evt.target.name] = evt.target.value.trim();
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 // console log for input data, checks if all the fields are filled out, reset localstorage and form data
 function onFormSubmit(e) {
   e.preventDefault();
-  console.log({ email: email.value, message: message.value });
+  // console.log({ email: email.value, message: message.value });
 
-  if (email.value === '' || message.value === '') {
-    return alert('Please fill out all the fields! :)');
-  }
-
+  // if (email.value === '' || message.value === '') {
+  //   return alert('Please fill out all the fields! :)');
+  // }
+  console.log(fomData);
   localStorage.removeItem(STORAGE_KEY);
   e.currentTarget.reset();
   formData = {};
 }
-
-// uses for populate data after reloading
-function populateInputData() {
-  if (formData) {
-    email.value = formData.email || '';
-    message.value = formData.message || '';
+const onLoad = () => {
+  try {
+     const data = localStorage.getItem(STORAGE_KEY);
+     if (!data) return;
+    formData = JSON.parse(data);
+    Object.entries(formData).forEach(([key, val]) => {
+       formEl.elements [key].value = val;
+  });
+  } catch (error) {
+  console.log(error.message);
   }
-}
+  };
+   window.addEventListener("load", onLoad);
+// uses for populate data after reloading
+// function populateInputData() {
+//   if (formData) {
+//     email.value = formData.email || '';
+//     message.value = formData.message || '';
+//   }
+// }
